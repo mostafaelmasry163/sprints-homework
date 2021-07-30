5ae9bb1c74528168834f14fed4289134b9a0294c
mostafaelmasry163 <mostelmasry93@gmail.com>
2021-07-30 20:16:57 +0200

    solving task 11

--- Python/S_PY_11.py
+++ Python/S_PY_11.py
@@ -5,3 +5,19 @@
 # For example, if the string is 'abdulrahman' then the output is:
 
 # Longest substring in alphabetical order is: abdu
+
+longString = input("enter a long string of characters : ")
+long = longString[0]
+current = longString[0]
+
+
+for char in longString[1:]:
+    if char >= current[-1]:
+        current += char
+        if len(current) > len(long):
+            long = current
+    else:
+        current = char
+
+
+print ("Longest substring in alphabetical order is:", long)
