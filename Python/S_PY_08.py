e9525df0fb48a1d6974598918dd3c59df84a0e44
mostafaelmasry163 <mostelmasry93@gmail.com>
2021-07-30 20:06:05 +0200

    add another solution

--- Python/S_PY_08.py
+++ Python/S_PY_08.py
@@ -2,14 +2,25 @@
 
 strin = input("enter a word : ")
 
-def reve_str (str) :
-    revestrin =""
-    leng = len(str)
-    while leng > 0 :
-        revestrin += str[leng-1]
-        leng -=1
+# def reve_str (str) :
+#     revestrin =""
+#     leng = len(str)
+#     while leng > 0 :
+#         revestrin += str[leng-1]
+#         leng -=1
 
 
-    return revestrin
+#     return revestrin
 
-print("the string reversed is "+reve_str(strin))
+# print("the string reversed is "+reve_str(strin))
+
+
+### Another solution
+
+def reverse_string(word) :
+    if word == "" : return "" 
+    last = word[-1]
+    els = word[0:-1]
+    return last + reverse_string(els)
+
+print("the string reversed is " + reverse_string(strin))
