d30dc48e174e6babd830d2507f0f4e9bf1caf953
mostafaelmasry163 <mostelmasry93@gmail.com>
2021-07-30 15:15:28 +0200

    solving task 6

--- Python/S_PY_06.py
+++ Python/S_PY_06.py
@@ -1,1 +1,14 @@
 #  Write a function that accepts two arguments (length, start) to generate an array of a specific length filled with integer numbers increased by one from start.
+
+leng=int(input("enter length of the array: "))
+start=int(input("enter the starting number: "))
+
+def array_generator(leng,start) :
+    arr=[start]
+    i=1
+    while i<leng :
+        arr.append(start+i)
+        i+=1
+    return arr
+
+print(array_generator(leng, start))
